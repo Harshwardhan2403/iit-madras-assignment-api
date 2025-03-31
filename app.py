ccb6558be5b5cbc711dee71254cd3f86fdd1053e
Harshwardhan Wandeshkar <112456163+Harshwardhan2403@users.noreply.github.com>
2025-03-31 22:56:47 +0530

    new

--- app.py
+++ app.py
@@ -1,26 +1,13 @@
 from flask import Flask, request, jsonify
-from qa_data import qa_pairs
-from difflib import get_close_matches
+from qa_data import qa_data
 
 app = Flask(__name__)
-
-def get_best_match(question):
-    questions = list(qa_pairs.keys())
-    matches = get_close_matches(question, questions, n=1, cutoff=0.5)  # Adjust cutoff for sensitivity
-    if matches:
-        return matches[0]
-    return None
 
 @app.route('/api/', methods=['POST'])
 def get_answer():
     question = request.form.get('question')
-    print(f"Received question: {question}")
-    
-    best_match = get_best_match(question)
-    
-    if best_match:
-        answer = qa_pairs[best_match]
-        return jsonify({"answer": answer})
+    if question in qa_data:
+        return jsonify({"answer": qa_data[question]})
     else:
         return jsonify({"answer": "Question not found."})
 
